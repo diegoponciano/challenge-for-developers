d315866ac29eb4c2ca3e394627ec098ea52699d4
Diego Ponciano <dgo.ponciano@gmail.com>
2017-10-15 12:48:09 -0200

    Docker bootstrap

--- python-django-skel/githubstars/settings.py
+++ python-django-skel/githubstars/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+import environ
 import os
+
+env = environ.Env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +28,7 @@ SECRET_KEY = 'a!x2$p3sf!ghrw@wwu_$*$b)(cogfbl6u6ghti4_r^_5&u+ds*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -74,10 +77,7 @@ WSGI_APPLICATION = 'githubstars.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db()
 }
 
 
