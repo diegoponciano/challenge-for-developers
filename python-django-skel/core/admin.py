bc1c52c7517a07ab35da7544ee6719368763dd68
Diego Ponciano <dgo.ponciano@gmail.com>
2017-10-20 02:14:14 -0200

    Register Repo model on admin

--- python-django-skel/core/admin.py
+++ python-django-skel/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import GithubRepo
+
+
+class GithubRepoAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(GithubRepo, GithubRepoAdmin)
