8c07ad3085b4c5b0cf58b40021d1ec64447f1d9a
Diego Ponciano <dgo.ponciano@gmail.com>
2017-10-23 18:36:25 -0200

    Replaced hardcoded for translation strings

--- python-django-skel/core/models.py
+++ python-django-skel/core/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import ugettext as _
 from urllib.parse import urlparse
 
 
@@ -26,19 +27,25 @@ class RepoManager(models.Manager):
 
 class GithubRepo(models.Model):
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        verbose_name=_('user')
     )
-    repo_id = models.CharField(max_length=100)
-    name = models.CharField(max_length=250)
-    url = models.URLField()
-    language = models.CharField(max_length=100)
+    repo_id = models.CharField(
+        max_length=100, verbose_name=_('repository ID'))
+    name = models.CharField(max_length=250, verbose_name=_('name'))
+    url = models.URLField(verbose_name=_('URL'))
+    language = models.CharField(max_length=100, verbose_name=_('language'))
     tags = ArrayField(models.CharField(max_length=100, blank=True), null=True)
 
     objects = RepoManager()
 
+    class Meta:
+        verbose_name = _('repository')
+        verbose_name_plural = _('repositories')
+
     def clean(self):
         if len(self.tags) != len(set(self.tags)):
-            raise ValidationError('Não são permitidas tags duplicadas.')
+            raise ValidationError(_('Duplicate tags are not allowed.'))
 
     def __str__(self):
         parts = urlparse(self.url)
