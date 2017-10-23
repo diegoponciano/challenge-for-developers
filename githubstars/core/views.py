96503fa557a90ae70b2ed35de2d183840502fd8f
Diego Ponciano <dgo.ponciano@gmail.com>
2017-10-23 19:59:47 -0200

    View to serve ajax search

--- githubstars/core/views.py
+++ githubstars/core/views.py
@@ -1,9 +1,20 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+
 from core.models import GithubRepo
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def busca(request):
+    query = request.GET.get('q')
+    if query:
+        results = GithubRepo.objects.search_tag_like(query)
+    else:
+        results = GithubRepo.objects.none()
+    return render(request, 'search.html', {'results': results})
 
 
 def search(request):
@@ -12,4 +23,5 @@ def search(request):
         results = GithubRepo.objects.search_tag_like(query)
     else:
         results = GithubRepo.objects.none()
-    return render(request, 'search.html', {'results': results})
+    return JsonResponse(
+        {'results': list(results.values('id', 'repo_id', 'name', 'url', 'language'))})
--- githubstars/tests/core/test_views.py
+++ githubstars/tests/core/test_views.py
@@ -26,3 +26,27 @@ def test_search_results(client):
     response = client.get('/busca?q=groovy')
     assert b'asd/qwe' not in response.content
     assert b'zxc/vbn' in response.content
+
+
+@pytest.mark.django_db
+def test_ajax_search_results(client):
+    mommy.make(GithubRepo, url='https://github.com/asd/qwe', tags=['elixir'])
+    mommy.make(GithubRepo, url='https://github.com/zxc/vbn', tags=['groovy'])
+
+    response = client.get('/search?q=elixir')
+    results = response.json()['results']
+    assert any(
+        [r for r in results if r['url'] == 'https://github.com/asd/qwe']
+    )
+    assert not any(
+        [r for r in results if r['url'] == 'https://github.com/zxc/vbn']
+    )
+
+    response = client.get('/search?q=groovy')
+    results = response.json()['results']
+    assert not any(
+        [r for r in results if r['url'] == 'https://github.com/asd/qwe']
+    )
+    assert any(
+        [r for r in results if r['url'] == 'https://github.com/zxc/vbn']
+    )
