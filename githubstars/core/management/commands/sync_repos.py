7173bc2346602db88905be3816505ba88890b831
Diego Ponciano <dgo.ponciano@gmail.com>
2017-10-23 21:04:41 -0200

    Performing search using ajax

--- githubstars/core/management/commands/sync_repos.py
+++ githubstars/core/management/commands/sync_repos.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         username = options['username']
         if not get_user_model().objects.filter(username=username).exists():
             raise CommandError(
-                'Usuário "%s" ainda não existe. Veja o comando createuser.'
+                'Usuário "%s" ainda não existe. Veja o comando createsuperuser.'
                 % username)
         cli = GithubClient(username)
         cli.sync_repos()
